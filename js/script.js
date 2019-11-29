ff1decbe6f9235f579442bb94fcb9003829a8d38
Andrew-1000 <andruambia95@gmail.com>
2019-11-29 13:00:25 +0300

    Partly finalize interface design

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Initializing days of the week with an array of 7 items
+day = new makeArray(7);
+day[0] = "Sunday";
+day[1] = "Monday";
+day[2] = "Tuesday";
+day[3] = "Wednesday";
+day[4] = "Thursday";
+day[5] = "Friday";
+day[6] = "Saturday";

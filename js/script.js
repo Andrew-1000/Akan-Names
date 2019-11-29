db8e385d1091c8026e6e6d0b632226d44a4994c1
Andrew-1000 <andruambia95@gmail.com>
2019-11-29 16:16:32 +0300

    Add an Array of names to the males and females variables

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Validating inputs 
 
-function formValidation(){
+function validateForm(){
   var cent = document.forms["akamGen"]["century"].value;
   var year = document.forms["akamGen"]["year"].value;
   var month = document.forms["akamGen"]["month"].value;
@@ -11,13 +11,13 @@ function formValidation(){
     alert("Century Digit Must be between 18 and 21 ");
     return false;
   } else if ( year == "" || year < 1900 || year >2019) {
-    alert("Sorry you dont exist");
+    alert("Year provided is wrong: Sorry you dont exist");
     return false;
   } else if (month == "" || month <= 0 || month >12) {
-    alert ("Enter value between 0 and 13");
+    alert ("Month is wrong: Enter value between 0 and 13");
     return false;
   } else if (day == "" || day <=0 || day > 31) {
-    alert ("Enter a number between 0 and 32");
+    alert ("Day is wrong: Enter a number between 0 and 32");
     return false;
   } else if (gender == "") {
     alert ("Please select gender");
@@ -25,17 +25,10 @@ function formValidation(){
     alert ("You have made it")
   }
 
+  var males = ["Akufo", "Ayuko", ""]
+  var females = ["Ayud", "Yaa", "Star"]
+
+  
+
 }
 
-
-
-
-//Initializing days of the week with an array of 7 items
-day = new makeArray(7);
-day[0] = "Sunday";
-day[1] = "Monday";
-day[2] = "Tuesday";
-day[3] = "Wednesday";
-day[4] = "Thursday";
-day[5] = "Friday";
-day[6] = "Saturday";

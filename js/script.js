0440800e04aa3a6fed61e1258ad33f176869ed39
Andrew-1000 <andruambia95@gmail.com>
2019-11-29 15:37:42 +0300

    Update the script.js file with more nested if statements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 //Validating inputs 
 
 function formValidation(){
-  var cent = document.forms["akamGender"]["century"].value;
-  var year = document.forms["akamGender"]["year"].value;
+  var cent = document.forms["akamGen"]["century"].value;
+  var year = document.forms["akamGen"]["year"].value;
+  var month = document.forms["akamGen"]["month"].value;
+  var day = document.forms["akamGen"]["day"].value;
+  var gender = document.forms["akamGen"]["gender"].value
+
+  if (cent == "" || cent < 19 || cent >21) {
+    alert("Century Digit Must be between 18 and 21 ");
+    return false;
+  } else if ( year == "" || year < 1900 || year >2019) {
+    alert("Sorry you dont exist");
+    return false;
+  } else if (month == "" || month <= 0 || month >12) {
+    alert ("Enter value between 0 and 13");
+    return false;
+  } else if (day == "" || day <=0 || day > 31) {
+    alert ("Enter a number between 0 and 32");
+    return false;
+  } else if (gender == "") {
+    alert ("Please select gender");
+  } else {
+    alert ("You have made it")
+  }
 
 }
 

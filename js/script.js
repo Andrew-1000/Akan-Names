26cee933468176371a2fb2213f6f6e73102d7504
Andrew-1000 <andruambia95@gmail.com>
2019-11-30 14:23:55 +0300

    Make changes to the script.js file and remove errors that prevent code from executing

--- js/script.js
+++ js/script.js
@@ -1,45 +1,79 @@
-//Validating inputs 
+function validateForm() {
+  
+  var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-function validateForm(){
-  var cent = document.forms["akamGen"]["century"].value;
-  var year = document.forms["akamGen"]["year"].value;
-  var month = document.forms["akamGen"]["month"].value;
-  var day = document.forms["akamGen"]["day"].value;
-  var gender = document.forms["akamGen"]["gender"].value
+  var cent = parseInt(document.getElementById("cent").value);
+  var year = parseInt(document.getElementById("year").value);
+  var month = parseInt(document.getElementById("month").value);
+  var day = parseInt(document.getElementById("day").value);
+  
+    
 
-  if (cent == "" || cent < 19 || cent >21) {
-    alert("Century Digit Must be between 18 and 21 ");
-    return false;
-  } else if ( year == "" || year < 1900 || year >2019) {
-    alert("Year provided is wrong: It should be between 1890-2020");
-    return false;
-  } else if (month == "" || month <= 0 || month >12) {
+  if (month == "" || month <= 0 || month >12) {
     alert ("Month is wrong: Enter value between 0 and 13");
     return false;
-  } else if (day == "" || day <=0 || day > 31) {
+  }
+   if (day == "" || day <=0 || day > 31) {
     alert ("Day is wrong: Enter a number between 0 and 32");
     return false;
   } 
-
-  var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   
-  var days = parseInt(( ( (cent/4) -2*cent-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
+  
+  var days = parseInt(cent / 4 - 2 * cent - 1 + (5 * year) / 4 + (26 * (month + 1)) / 10 + day) % 7;
 
-  var checkGender = document.querySelector('input[name="gender"]: checked').value;
+  var gender = document.querySelector('input[name="gender"]:checked').value;
 
-  if (checkGender=== 1){
-    if (days === 0){
+  if (gender == 1) {
+    if (days === 0) {
       alert("Sunday was your day of birth");
-      alert( males[0] + "Is your name");
-    } else if (day === 1) {
+      alert( "Your name should be" + females[0] );
+    } else if (days === 1) {
       alert("You were born on Monday");
-      alert("Your name is" + males[1]);
-    } else if (day === 2) {
+      alert("Your name is" + females[1]);
+    } else if (days === 2) {
       alert("Tuesday is when you were born");
-      alert("You are" + males[2]);
+      alert("You are" + females[2]);
+    } else if (days === 3) {
+      alert("Wednesday is when you were born");
+      alert("Your name should be: " + females[3]);
+    } else if (days === 4) {
+      alert("Thursday is when you were born");
+      alert("Your name should be: " + females[4]);
+    } else if (days === 5) {
+      alert("Friday is when you were born");
+      alert("Your name should be: " + females[5]);
+    } else if (days === 6) {
+      alert("Saturday is when you were born");
+      alert("Your name should be: " + females[6]);
+    } else {
+      alert ("Kindly try Again Later");
     }
+  } else {
+    if (days === 0) {
+      alert ("Your were born on Sunday");
+      alert ("Your name should be: " + males[0]);
+    } else if (days === 1) {
+      alert ("You were born on Monday");
+      alert ("Your name should be: " + males[1]);
+    } else if (days === 2) {
+      alert ("You were born on Tuesday");
+      alert ("Your name should be: " + males[2]);
+    } else if (days === 3) {
+      alert ("You were born on Wednesday");
+      alert ("Your name should be: " + males[3]);
+    } else if (days === 4) {
+      alert ("You were born on Thursday");
+      alert ("Your name should be: " + males[4]);
+    } else if (days === 5) {
+      alert ("You were born on Friday");
+      alert ("Your name should be: " + males[5]);
+    } else if (days === 6) {
+      alert ("You were born on Saturday");
+      alert ("Your name should be: " + males[6]);
+    } 
   }
+} 
 
-}
+
 
